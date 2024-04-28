907780ab79df368c7b0fe2c6098b311d508bcb15
TasfiqJ <jasimuddintasfiq@gmail.com>
2024-04-28 11:43:06 -0400

    Parsing given PDF and answering questios based on it

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = Ollama(model="mistral", request_timeout=30.0)
 
-result = llm.complete("Hello")
+parser = LlamaParse(result_type="markdown")
+
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+query_engine = vector_index.as_query_engine(llm=llm)
+
+result = query_engine.query("What are some of the routes in the api?")
 print(result)
